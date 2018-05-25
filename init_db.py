f75b02ac7673cda251d08c89f772e98ad03b951c
mahont1 <thomas.mahoney@ext.ons.gov.uk>
2018-05-25 11:34:13 +0100

    Rename run_status.csv to run_steps.csv
    File renames and references updated.

--- init_db.py
+++ init_db.py
@@ -35,7 +35,7 @@ def main():
         db.session.commit()
 
         # Load in the data
-        reader3 = csv.DictReader(open('resources/run_status.csv'))
+        reader3 = csv.DictReader(open('resources/run_steps.csv'))
         for record in reader3:
             # Make use of Pythons dictionary unpacking
             # https://docs.python.org/3/tutorial/controlflow.html#unpacking-argument-lists
