a2e8b2be1b15816eb8c4743302b50ea31a99c7cc
mahont1 <thomas.mahoney@ext.ons.gov.uk>
2018-11-23 11:58:01 +0000

    Modify get runs to use SQL server.

--- myapp/runs.py
+++ myapp/runs.py
@@ -8,23 +8,44 @@ bp = Blueprint('runs', __name__, url_prefix='/runs', static_folder='static')
 # RUNS
 
 
+@bp.route('', methods=['GET'])
+@bp.route('/<run_id>', methods=['GET'])
+def get_runs(run_id=None):
+    output = b_logic_get_run()
+
+    return output
+
+
 @bp.route('', methods=['POST'])
 def create_run():
-    # the request should be json and an id must be present
-    if not request.json or 'id' not in request.json:
-        abort(400)
-
-    run = Run(id=request.json['id'],
-              name=request.json['name'],
-              desc=request.json.get('desc', ""),
-              start_date=request.json.get('start_date', ""),
-              end_date=request.json.get('end_date', ""),
-              status=request.json.get('status', ""),
-              type=request.json.get('type', ""))
-    db.session.add(run)
-    db.session.commit()
+    b_logic_create_run()
 
     return "", 201
+
+
+
+@bp.route('/<run_id>', methods=['PUT'])
+def update_run(run_id):
+    # the request should be json and an id must be present
+    if not request.json or not run_id:
+        abort(400)
+
+    data = Run.query.get(run_id)
+
+    if not data:
+        abort(400)
+
+    data.name = request.json['name']
+    data.desc = request.json.get('desc', "")
+    data.start_date = request.json.get('start_date', "")
+    data.end_date = request.json.get('end_date', "")
+    data.status = request.json.get('status', "")
+    data.type = request.json.get('type', "")
+
+    db.session.commit()
+
+    return "", 200
+
 
 
 # @bp.route('', methods=['GET'])
@@ -53,46 +74,15 @@ def create_run():
 #     return jsonify(output)
 
 
-@bp.route('/<run_id>', methods=['PUT'])
-def update_run(run_id):
-    # the request should be json and an id must be present
-    if not request.json or not run_id:
-        abort(400)
-
-    data = Run.query.get(run_id)
-
-    if not data:
-        abort(400)
-
-    data.name = request.json['name']
-    data.desc = request.json.get('desc', "")
-    data.start_date = request.json.get('start_date', "")
-    data.end_date = request.json.get('end_date', "")
-    data.status = request.json.get('status', "")
-    data.type = request.json.get('type', "")
-
-    db.session.commit()
-
-    return "", 200
-
-
-
-
-
-
-@bp.route('', methods=['GET'])
-@bp.route('/<run_id>', methods=['GET'])
-def get_runs(run_id=None):
-    output = b_logic_get_run()
-
-    return output
-
 # Logic
+
+
+import myapp.persistence_layer as p_layer
 
 
 def b_logic_get_run(run_id=False):
 
-    data = p_layer_get('RUN')
+    data = p_layer.get('RUN')
 
     column_names = data.columns
     output = None
@@ -112,14 +102,22 @@ def b_logic_get_run(run_id=False):
 
     return output
 
+def b_logic_create_run():
+    # the request should be json and an id must be present
+    if not request.json or 'id' not in request.json:
+        abort(400)
 
-def p_layer_get(table_name):
-    conn = get_connection()
 
-    # Create SQL statement
-    sql = "SELECT * from " + table_name
+    
+    run = Run(id=request.json['id'],
+              name=request.json['name'],
+              desc=request.json.get('desc', ""),
+              start_date=request.json.get('start_date', ""),
+              end_date=request.json.get('end_date', ""),
+              status=request.json.get('status', ""),
+              type=request.json.get('type', ""))
+    db.session.add(run)
+    db.session.commit()
 
-    # Execute the sql statement using the pandas.read_sql function and return
-    # the result.
-    import pandas
-    return pandas.read_sql(sql, conn)
+    return "", 201
+
